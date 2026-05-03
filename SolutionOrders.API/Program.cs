618c60c3789eab5c84e79ae736c36ddcb0393560
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-03 21:53:15 +0200

    feat(Lecture02): init of API

--- SolutionOrders.API/Program.cs
+++ SolutionOrders.API/Program.cs
@@ -19,6 +19,10 @@ namespace SolutionOrders.API
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+                });
             }
 
             app.UseHttpsRedirection();
