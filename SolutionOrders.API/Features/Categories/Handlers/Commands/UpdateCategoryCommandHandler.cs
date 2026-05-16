bc6c252930eceabf6ee69eb1eddaaa2ffa6af852
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-16 19:43:30 +0200

    feat(Lecture6): Add UpdateCategory command handler and message structure

---
+++ SolutionOrders.API/Features/Categories/Handlers/Commands/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SolutionOrders.API.Features.Categories.Messages.Commands;
+using SolutionOrders.API.Models.Data;
+
+namespace SolutionOrders.API.Features.Categories.Handlers.Commands
+{
+    public class UpdateCategoryCommandHandler(ApplicationDbContext context)
+        : IRequestHandler<UpdateCategoryCommand, Unit>
+    {
+        public async Task<Unit> Handle(
+            UpdateCategoryCommand request,
+            CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(request.Name))
+            {
+                throw new ArgumentException("Nazwa kategorii jest wymagana");
+            }
+
+            var category = await context.Categories
+                .FirstOrDefaultAsync(category =>
+                        category.IdCategory == request.IdCategory &&
+                        category.IsActive,
+                    cancellationToken);
+
+            if (category == null)
+            {
+                throw new KeyNotFoundException(
+                    $"Kategoria o ID {request.IdCategory} nie istnieje");
+            }
+
+            category.Name = request.Name;
+            category.Description = request.Description;
+            category.IsActive = request.IsActive;
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ SolutionOrders.API/Features/Categories/Messages/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace SolutionOrders.API.Features.Categories.Messages.Commands
+{
+    public class UpdateCategoryCommand : IRequest<Unit>
+    {
+        public int IdCategory { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
