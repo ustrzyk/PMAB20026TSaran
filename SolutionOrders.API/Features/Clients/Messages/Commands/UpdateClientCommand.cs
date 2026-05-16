cbdcb012091f5a958cf5236c116c0c6d81892de0
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-16 22:44:36 +0200

    feat(Lecture6): Implement UpdateClient command, handler, and controller method for updating client details

---
+++ SolutionOrders.API/Features/Clients/Messages/Commands/UpdateClientCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace SolutionOrders.API.Features.Clients.Messages.Commands
+{
+    public class UpdateClientCommand : IRequest<Unit>
+    {
+        public int IdClient { get; set; }
+        public string? Name { get; set; }
+        public string? Adress { get; set; }
+        public string? PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
