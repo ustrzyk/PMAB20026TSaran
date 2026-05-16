4a5b146bc595a62cba8a49dbe62fc25a20fcd80c
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-16 20:56:49 +0200

    feat(Lecture6): Add UpdateUnitOfMeasurement command, handler, and controller method for updating unit of measurement

---
+++ SolutionOrders.API/Features/UnitOfMeasurements/Messages/Commands/UpdateUnitOfMeasurementCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace SolutionOrders.API.Features.UnitOfMeasurements.Messages.Commands
+{
+    public class UpdateUnitOfMeasurementCommand : IRequest<Unit>
+    {
+        public int IdUnitOfMeasurement { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
