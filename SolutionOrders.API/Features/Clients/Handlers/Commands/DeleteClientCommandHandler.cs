5d773715caced0f6512bbb32dae65714e48d35a2
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-16 22:53:32 +0200

    feat(Lecture6): Implement DeleteClient command, handler, and controller method for soft deletion of clients

---
+++ SolutionOrders.API/Features/Clients/Handlers/Commands/DeleteClientCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SolutionOrders.API.Features.Clients.Messages.Commands;
+using SolutionOrders.API.Models.Data;
+
+namespace SolutionOrders.API.Features.Clients.Handlers.Commands
+{
+    public class DeleteClientCommandHandler(ApplicationDbContext context)
+        : IRequestHandler<DeleteClientCommand, Unit>
+    {
+        public async Task<Unit> Handle(
+            DeleteClientCommand request,
+            CancellationToken cancellationToken)
+        {
+            var client = await context.Clients
+                .FirstOrDefaultAsync(client =>
+                        client.IdClient == request.IdClient &&
+                        client.IsActive,
+                    cancellationToken);
+
+            if (client == null)
+            {
+                throw new KeyNotFoundException(
+                    $"Klient o ID {request.IdClient} nie istnieje");
+            }
+
+            var hasOrders = await context.Orders
+                .AnyAsync(order =>
+                        order.IdClient == request.IdClient,
+                    cancellationToken);
+
+            if (hasOrders)
+            {
+                throw new InvalidOperationException(
+                    "Nie można usunąć klienta, ponieważ ma przypisane zamówienia");
+            }
+
+            client.IsActive = false;
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
