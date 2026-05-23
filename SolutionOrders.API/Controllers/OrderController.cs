4afc93ea4b8a624cae264932bb9805274ef1a27b
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-23 21:46:11 +0200

    feat(Lecture07): Implement Order management queries and controller for retrieving orders

---
+++ SolutionOrders.API/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SolutionOrders.API.Features.Orders.Messages.DTOs;
+using SolutionOrders.API.Features.Orders.Messages.Queries;
+
+namespace SolutionOrders.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderDto>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            // Tworzymy Query
+            var query = new GetAllOrdersQuery();
+
+            // Wysyłamy do MediatR
+            return Ok(await mediator.Send(query));
+        }
+
+        /// <summary>
+        /// Pobiera zamówienie po ID
+        /// </summary>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(OrderDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetOrderByIdQuery(id);
+            var result = await mediator.Send(query);
+
+            if (result == null)
+            {
+                return NotFound(new { message = $"Zamówienie o ID {id} nie zostało znalezione" });
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ SolutionOrders.API/Features/Orders/Messages/Queries/GetAllOrdersQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SolutionOrders.API.Features.Orders.Messages.DTOs;
+
+namespace SolutionOrders.API.Features.Orders.Messages.Queries
+{
+    public class GetAllOrdersQuery : IRequest<IEnumerable<OrderDto>>
+    {
+    }
+}
