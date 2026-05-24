cd360753b88da0f45a5c1ad4a4983b90f5d62610
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-24 12:52:49 +0200

    feat(Lecture9): Implement checkout process with order creation and related DTOs

---
+++ SolutionOrders.API/Controllers/CheckoutController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SolutionOrders.API.Features.Checkout.Messages.Commands;
+using SolutionOrders.API.Features.Checkout.Messages.DTOs;
+
+namespace SolutionOrders.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckoutController(IMediator mediator) : ControllerBase
+    {
+        /// <summary>
+        /// Finalizuje koszyk i tworzy zamówienie klienta
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(typeof(CheckoutOrderResponseDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateOrderFromCart(
+            [FromBody] CreateCheckoutOrderCommand command)
+        {
+            try
+            {
+                var result = await mediator.Send(command);
+
+                return Created(
+                    $"/api/Order/{result.IdOrder}",
+                    result
+                );
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
