bf60538a92723261a311d034cf32bcb37668490e
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-05-24 09:07:59 +0200

    feat(Lecture08): Add GetOrderItemsByOrderId query and corresponding handler to retrieve order items by order ID

---
+++ SolutionOrders.API/Features/OrderItems/Messages/Queries/GetOrderItemsByOrderIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SolutionOrders.API.Features.OrderItems.Messages.DTOs;
+
+namespace SolutionOrders.API.Features.OrderItems.Messages.Queries
+{
+    public class GetOrderItemsByOrderIdQuery : IRequest<IEnumerable<OrderItemDto>>
+    {
+        public int IdOrder { get; set; }
+
+        public GetOrderItemsByOrderIdQuery(int idOrder)
+        {
+            IdOrder = idOrder;
+        }
+    }
+}
